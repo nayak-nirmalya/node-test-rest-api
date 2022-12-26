85def8a6b5a03ef429e137a4c6958576562a4c0b
Nirmalya Nayak <n.nayak263@gmail.com>
2022-12-26 13:52:36 +0530

    added sinon npm package for adding stub

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -1,4 +1,6 @@
 const expect = require('chai').expect
+const jwt = require('jsonwebtoken')
+const sinon = require('sinon')
 
 const authMiddleware = require('../middleware/is-auth')
 
@@ -18,6 +20,29 @@ describe('Auth Middleware', () => {
     const req = {
       get: function () {
         return 'STRING'
+      },
+    }
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw()
+  })
+
+  it('should yield a userId after decoding the token', () => {
+    const req = {
+      get: function (headerName) {
+        return 'Bearer STRING_VALID'
+      },
+    }
+    sinon.stub(jwt, 'verify')
+    jwt.verify.returns({ userId: 'user-id-valid' })
+    authMiddleware(req, {}, () => {})
+    expect(req).to.have.a.property('userId')
+    expect(req).to.have.a.property('userId', 'user-id-valid')
+    jwt.verify.restore()
+  })
+
+  it('should thow an error if the token can not be verified', () => {
+    const req = {
+      get: function () {
+        return 'Bearer STRING'
       },
     }
     expect(authMiddleware.bind(this, req, {}, () => {})).to.throw()
