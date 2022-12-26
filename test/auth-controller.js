522474774f5a230b43bbad8a4456467628e1517a
Nirmalya Nayak <n.nayak263@gmail.com>
2022-12-26 14:47:55 +0530

    testing auth controller getUserStatus method

--- test/auth-controller.js
+++ test/auth-controller.js
@@ -1,5 +1,6 @@
 const expect = require('chai').expect
 const sinon = require('sinon')
+const mongoose = require('mongoose')
 
 const User = require('../models/user')
 const AuthController = require('../controllers/auth')
@@ -22,5 +23,44 @@ describe('Auth Controller - LogIn', () => {
     })
 
     User.findOne.restore()
+  })
+
+  it('should send a response with valid user status for an existing user', (done) => {
+    mongoose
+      .connect(
+        'mongodb+srv://nirmalya:nirmalya@cluster.a9tjk7u.mongodb.net/test-message',
+      )
+      .then((result) => {
+        const user = new User({
+          _id: '639c64828560d31be0576683',
+          email: 'test@test.com',
+          password: 'password',
+          name: 'Test',
+          posts: [],
+        })
+        return user.save()
+      })
+      .then(() => {
+        const req = {
+          userId: '639c64828560d31be0576683',
+        }
+        const res = {
+          statusCode: 500,
+          userStatus: null,
+          status: function (code) {
+            this.statusCode = code
+            return this
+          },
+          json: function (data) {
+            this.userStatus = data.status
+          },
+        }
+        AuthController.getUserStatus(req, res, () => {}).then(() => {
+          expect(res.statusCode).to.be.equal(200)
+          expect(res.userStatus).to.be.equal('I am new!')
+          done()
+        })
+      })
+      .catch((err) => console.log(err))
   })
 })
