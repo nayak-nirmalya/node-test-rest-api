e83aff11e5cc3d0109542f2585d7c5df0d8b8870
Nirmalya Nayak <n.nayak263@gmail.com>
2022-12-26 13:31:22 +0530

    testing jwt token

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -2,13 +2,24 @@ const expect = require('chai').expect
 
 const authMiddleware = require('../middleware/is-auth')
 
-it('should throw an error if no authorization header is present', () => {
-  const req = {
-    get: function () {
-      return null
-    },
-  }
-  expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
-    'Not authenticated.',
-  )
+describe('Auth Middleware', () => {
+  it('should throw an error if no authorization header is present', () => {
+    const req = {
+      get: function () {
+        return null
+      },
+    }
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
+      'Not authenticated.',
+    )
+  })
+
+  it('should throw an error if the authorization header is only one string', () => {
+    const req = {
+      get: function () {
+        return 'STRING'
+      },
+    }
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw()
+  })
 })
