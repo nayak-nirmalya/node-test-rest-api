d2f909252b181f6d901be8c7b8dc13e255a85619
Nirmalya Nayak <n.nayak263@gmail.com>
2022-12-26 14:56:32 +0530

    code clean up with befor and after hooks

--- test/auth-controller.js
+++ test/auth-controller.js
@@ -5,7 +5,25 @@ const mongoose = require('mongoose')
 const User = require('../models/user')
 const AuthController = require('../controllers/auth')
 
-describe('Auth Controller - LogIn', () => {
+describe('Auth Controller', () => {
+  before(function (done) {
+    mongoose
+      .connect(
+        'mongodb+srv://nirmalya:nirmalya@cluster.a9tjk7u.mongodb.net/test-message',
+      )
+      .then((result) => {
+        const user = new User({
+          _id: '639c64828560d31be0576683',
+          email: 'test@test.com',
+          password: 'password',
+          name: 'Test',
+          posts: [],
+        })
+        return user.save()
+      })
+      .then(() => done())
+  })
+
   it('should throw an error with code 500 if accessing db fails', (done) => {
     sinon.stub(User, 'findOne')
     User.findOne.throws()
@@ -26,45 +44,32 @@ describe('Auth Controller - LogIn', () => {
   })
 
   it('should send a response with valid user status for an existing user', (done) => {
-    mongoose
-      .connect(
-        'mongodb+srv://nirmalya:nirmalya@cluster.a9tjk7u.mongodb.net/test-message',
-      )
-      .then((result) => {
-        const user = new User({
-          _id: '639c64828560d31be0576683',
-          email: 'test@test.com',
-          password: 'password',
-          name: 'Test',
-          posts: [],
-        })
-        return user.save()
-      })
+    const req = {
+      userId: '639c64828560d31be0576683',
+    }
+    const res = {
+      statusCode: 500,
+      userStatus: null,
+      status: function (code) {
+        this.statusCode = code
+        return this
+      },
+      json: function (data) {
+        this.userStatus = data.status
+      },
+    }
+    AuthController.getUserStatus(req, res, () => {}).then(() => {
+      expect(res.statusCode).to.be.equal(200)
+      expect(res.userStatus).to.be.equal('I am new!')
+      done()
+    })
+  })
+
+  after(function (done) {
+    User.deleteMany({})
       .then(() => {
-        const req = {
-          userId: '639c64828560d31be0576683',
-        }
-        const res = {
-          statusCode: 500,
-          userStatus: null,
-          status: function (code) {
-            this.statusCode = code
-            return this
-          },
-          json: function (data) {
-            this.userStatus = data.status
-          },
-        }
-        AuthController.getUserStatus(req, res, () => {}).then(() => {
-          expect(res.statusCode).to.be.equal(200)
-          expect(res.userStatus).to.be.equal('I am new!')
-          User.deleteMany({})
-            .then(() => {
-              return mongoose.disconnect()
-            })
-            .then(() => done())
-        })
+        return mongoose.disconnect()
       })
-      .catch((err) => console.log(err))
+      .then(() => done())
   })
 })
